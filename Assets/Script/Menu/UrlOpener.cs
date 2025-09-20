e3d700b948ebb11db4a1ed08bbddcd473bcbc54f
cocoshenrigari <cocoshenrigari@gmail.com>
2025-09-20 16:25:43 +0200

    final stretch

---
+++ Assets/Script/Menu/UrlOpener.cs
@@ -0,0 +1,19 @@
+
+using UnityEngine;
+
+
+    public class UrlOpener : MonoBehaviour
+    {
+        
+        
+        public string Url;
+
+
+    public void Open()
+
+      {
+        Application.OpenURL(Url);
+      }  
+
+    }
+
